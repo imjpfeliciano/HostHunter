e788a0732f926b118d6487b573f954bc42291647
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2015-10-18 01:21:41 -0500

    y siguen los cambios

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 var express = require('express');
 var mongoose = require('mongoose');
-//var cors = require('cors');
+var uriUtil = require('mongodb-uri');
 var morgan = require('morgan');             // log requests to the console (express4)
 var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 
-app.set('port', (process.env.PORT || 5000));
 
-mongoose.connect('mongodb://admin:admin@ds041164.mongolab.com:41164/hosthunter');
+var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } }; 
+
+
+var mongodbUri = 'mongodb://admin:hosthunter@ds041164.mongolab.com:41164/hosthunter';
+var mongooseUri = uriUtil.formatMongoose(mongodbUri);
+
+
+mongoose.connect(mongooseUri, options);
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
 
 var personSchema = {
 	firstName: String,
@@ -20,6 +31,7 @@ var Person = mongoose.model('Person', personSchema, 'people');
 var app = express();
 //app.use(cors());
 
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));                 // set the static files location /public/img will be /img for users
 app.use(morgan('dev'));                                         // log every request to the console
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
