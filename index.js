eee7df9dc38f5d3e9d0b26b8e0fe990f0aa318cf
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2015-10-21 18:55:42 -0500

    MongoLab Connection * Se crea la conexión con la base de datos en mongolab * consulta de usuarios registrados

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var uriUtil = require('mongodb-uri');
+
+var mongodbUri = 'mongodb://admin:hosthunter@ds041164.mongolab.com:41164/hosthunter';
+var mongooseUri = uriUtil.formatMongoose(mongodbUri);
+
+var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
+                replset: { socketOptions: { keepAlive: 1, connectTimeoutMS : 30000 } } }; 
+
+mongoose.connect(mongooseUri, options);
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+var personSchema = {
+	firstName: String,
+	lastName: String,
+	email: String
+}
+
+var Person = mongoose.model('Person', personSchema, 'people');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -10,13 +33,13 @@ app.use(express.static(__dirname + '/public'));
 //app.set('view engine', 'ejs');
 
 app.get('/list', function(request, response){
-	var data = [
- 				    {"firstName":"John", "lastName":"Doe", "email":"John@example.com"}, 
-    				{"firstName":"Anna", "lastName":"Smith", "email":"Anna@example.com"}, 
-				    {"firstName":"Peter","lastName":"Jones", "email": "Peter@example.com"}
-				];
-
-	response.json(data);
+	Person.find(function (error, data){
+		if(error){
+			response.send(error);
+		}
+		console.log(data);
+		response.json(data);
+	});
 });
 
 app.get('/', function(request, response) {
